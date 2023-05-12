c45f06e36f61186c41e624d47e22a4fb5b03217c
Emre ÇOLAK <colakkemre@gmail.com>
2023-05-12 19:41:03 +0300

    install other dependencies and add some configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,16 @@
 // Import Dependencies
 import express from 'express'
+import cors from 'cors'
 
 
 // Initialize express
 const app = express()
 
+
+// Middlewares
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+}))
 
 export {app}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,19 @@
+// Import Dependencies
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import {app} from './app'
 
 const start = () => {
 
-    app.listen(3000, () => {
-        console.log(`Server listening on ${3000}`)
+    if(!process.env.API_PORT) {
+        console.log('API_PORT must be defined')
+    }
+
+    const PORT = process.env.API_PORT
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`)
     })
 }
 
