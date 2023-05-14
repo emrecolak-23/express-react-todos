8aa4deb33bf2a1acf1713fee781f69afa8068bd6
Emre ÇOLAK <colakkemre@gmail.com>
2023-05-14 11:14:33 +0300

    add spinner component

--- client/src/routes/home/home.component.tsx
+++ client/src/routes/home/home.component.tsx
@@ -4,20 +4,21 @@ import { RootState } from "../../store";
 import { UserData } from "../../store/slices/auth-slice";
 import Todos from "../../components/todos/todos.component";
 import NewTodo from "../../components/new-todo/new-todo.component";
+import Spinner from "../../components/spinner/spinner.component";
+
 function Home() {
 
-    const currentUser = useSelector<RootState, UserData | null>(state => {
-        return state.auth.currentUser
+    const [currentUser, isLoading] = useSelector<RootState, [UserData | null, boolean]>(state => {
+        return [state.auth.currentUser, state.auth.isLoading]
     })
 
     let content;
 
     if(!currentUser) {
-       content = <h1>Welcome to Todo App</h1>
+       content = isLoading ? <Spinner/> : <h1>Welcome to Todo App</h1>
     } else {
         content = <><NewTodo/> <Todos/></>
     }
-
 
     return <HomeContainer>
       {content}
